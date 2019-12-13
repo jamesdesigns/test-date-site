5756279f6c26b9ca70827cfef150bd9d77e216eb
James Hooper <jameshooperdesigns@gmail.com>
2019-12-12 17:06:23 -0700

    Edit line 2 to having +6 characters and contain 123

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // JavaScript 
-let userEmail = 'lco12'
+let userEmail = 'abc1234'
 let password = '1234'
 
-console.log(password.length);
-console.log(userEmail.toLowerCase());
+let userChecker = function(myString) {
+    // userEmail must be greater than 6 characters AND contain 123
+    if ((myString.includes(123)) && (myString.length > 6)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(userChecker(userEmail));
